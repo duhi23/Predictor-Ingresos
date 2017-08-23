a338dc71d8ebeb2f14224eef8618284020eb1405
duhi23 <diego.21.phs@hotmail.com>
2017-08-22 22:40:44 -0500

    UPdate

--- clean_data.R
+++ clean_data.R
@@ -4,6 +4,10 @@
 
 library(haven)
 library(dplyr)
+library(readr)
+
+afi15 <- read_tsv("Afiliados Activos062015.txt")
+
 
 afi15 <- read_sav("Afiliados Activos062015.sav")
 afi15 %>% filter(DP_VALOR_SUELDO>10000)
