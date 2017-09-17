bde2d1661c019f2267da586507a951e40892d352
duhi23 <diego.21.phs@hotmail.com>
2017-09-17 23:35:34 +0200

    Update Clean Data

--- clean_data.R
+++ clean_data.R
@@ -8,9 +8,14 @@ library(readr)
 library(data.table)
 list.files()
 
-afi15 <- fread("Afiliados Activos062015.txt", sep = "|", dec = ",")
-afi16 <- fread("AFILIADOS_2016_COMPLETA.txt", sep = ";")
+afi15 <- read_sav("Afiliados_Activos062015.sav")
+afi16 <- read_sav("Afiliados_Activos052016.sav")
+colnames(afi16) <- c("DP_CEDULA", "SUELDO16")
+cons <- full_join(afi15, afi16, by="DP_CEDULA")
+# data para depurar en SPSS
+write_sav(cons, "data_consolidada.sav")
 
+# datos depurados
+data <- read_sav("data_consolidada.sav")
 
-afi15 <- read_sav("Afiliados Activos062015.sav")
-afi15 %>% filter(DP_VALOR_SUELDO>10000)
+data %>% select(DP_CEDULA, SALARIO_16, SALARIO_15) %>% filter(SALARIO_15<354) %>% summary()
